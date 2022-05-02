6c30aa5f7102a3111d1cd52d424c1304675b8246
LordDarkSider <98459317+LordDarkSider@users.noreply.github.com>
2022-05-02 21:49:52 +0200

    Add files via upload

--- scripts/distance.js
+++ scripts/distance.js
@@ -5,16 +5,16 @@ function deg2rad(x){
 
 function distance(A,B){
     ///Calcule la distance en m entre deux points sur Terre
-    ///A et B sont les couples (listes) de coordonnées (latitude, longitude) en degrés des 2 points
+    ///A et B sont les couples de coordonnées (latitude, longitude) en degrés des 2 points
 
     let r = 6378137; //rayon de la Terre en km
-    A[0] = deg2rad(A[0])
-    A[1] = deg2rad(A[1])
-    B[0] = deg2rad(B[0])
-    B[1] = deg2rad(B[1])
+    let x1 = deg2rad(A[0])
+    let y1 = deg2rad(A[1])
+    let x2 = deg2rad(B[0])
+    let y2 = deg2rad(B[1])
 
     //distance orthodromique (formule de haversine)
-    let d = 2* r* Math.asin(Math.sqrt( Math.pow(Math.sin((B[0]-A[0])/2),2) + ( Math.cos(A[0]) * Math.cos(B[0]) * Math.pow((Math.sin((B[1]-A[1])/2)),2) ) ));
+    let d = 2* r* Math.asin(Math.sqrt( Math.pow(Math.sin((x2-x1)/2),2) + ( Math.cos(x1) * Math.cos(x2) * Math.pow((Math.sin((y2-y1)/2)),2) ) ));
 
     return Math.round(d,2);
 };
